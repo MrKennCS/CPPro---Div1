26a09123e12a3ad02d497b45c79cd568219beeed
mrkenn <trungkientuyentinhoc@gmail.com>
2025-08-05 23:51:10 +0700

    Test

---
+++ Tuan5/bai1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n;  cin >> n;
+    cout << n + 1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);   cin.tie(0);
+    solve();
+}
