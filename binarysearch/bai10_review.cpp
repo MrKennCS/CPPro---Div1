534ff79f31667b3929cd4c9d4cfe13737f430a22
DrShaunMurphy <138607504+DrShaunMurphy@users.noreply.github.com>
2025-09-02 14:59:21 +0700

    Update from PC

---
+++ binarysearch/bai10_review.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define ll long long
+
+using namespace std;
+
+int n;
+ll k;
+int a[100009];
+int b[100009];
+
+void solve(){
+    cin >> n >> k;
+    for(int i=1; i<=n; ++i) cin >> a[i];
+    for(int i=1; i<=n; ++i) cin >> b[i];
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);   cin.tie(0);
+    solve();
+}
--- binarysearch/bai15.cpp
+++ binarysearch/bai15.cpp
@@ -1,16 +1,37 @@
 #include<iostream>
-
+#include<cmath>
+#define ll long long
 using namespace std;
 
 int n, m;
 int a[1000009];
+int l, r, mid, res;
 
-
+ll cnt(int x){
+    ll ans = 0;
+    for(int i=1; i<=m; ++i){
+        ans += ceil(1.0 * a[i] / x);
+    }
+    return ans;
+}
 
 void solve(){
     cin >> n >> m;
+    for(int i=1; i<=m; ++i) cin >> a[i];
 
-}
+    l = 1;
+    r = 1e9;
+
+    while(l <= r){
+        mid = (l + r) >> 1;
+        if(cnt(mid) <= n){
+            r = mid - 1;
+            res = mid;
+        }else   l = mid + 1;
+    }
+
+    cout << res;
+}  
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
