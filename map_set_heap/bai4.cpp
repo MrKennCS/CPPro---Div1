33b9b8b4814c756b02065da2273704846e552ca3
MrKenne <trungkientuyentinhoc@gmail.comm>
2026-04-06 14:35:26 +0700

    Update from laptop

--- map_set_heap/bai4.cpp
+++ map_set_heap/bai4.cpp
@@ -1,26 +1,89 @@
-#include<iostream>
-#include<set>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include<bits/stdc++.h>
 
 using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+
+#define sz(a) (int)a.size()
+#define getbit(x, y) (((x) >> (y)) & 1)
+#define turnon(x, y) ((x) | (1LL << y))
+#define turnof(x, y) ((x) ^ (1LL << y))
+#define foru(i, a, b)   for(int i=a; i<=b; ++i)
+#define ford(i, a, b)   for(int i=a; i>=b; --i)
+#define foruc(i, a, b, c)   for(int i=a; i<=b; i+=c)
+#define fordc(i, a, b, c)   for(int i=a; i>=b; i-=c)
+
+#define fi first
+#define se second
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define lb lower_bound // >=
+#define ub upper_bound // >
+
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+
+ll rand(ll l, ll r){
+    assert(l <= r);
+    return l + rd() % (r - l + 1);
+}
+
+void file(){
+    if(fopen("TEST.INP", "r")){
+        freopen("TEST.INP", "r", stdin);
+        freopen("TEST.OUT", "w", stdout);
+    }
+}
+
+const int mod = 1e9 + 7;
+int tc = 1;
+
+/*
+
+*/
+
+int n, m;
+int val;
+set<int> st1, st2;
+vector<int> res;
 
 void solve(){
-    int n, m;   cin >> n >> m;
-    set<int> st1;
-    set<int> st2;
-    for(int i=1; i<=n; ++i){
-        int tmp;    cin >> tmp;
-        st1.insert(tmp);
-    }
+    cin >> n >> m;
+    for(int i=1; i<=n; ++i) cin >> val, st1.insert(val);
     for(int i=1; i<=m; ++i){
-        int tmp;    cin >> tmp;
-        if(st1.find(tmp) != st1.end())  st2.insert(tmp);
+        cin >> val;
+        if(st1.find(val) != st1.end())  st2.insert(val);
     }
-    for(set<int>::iterator it=st2.begin(); it!=st2.end(); ++it){
-        cout << *it << " ";
-    }
+    for(set<int>::iterator it=st2.begin(); it!=st2.end(); ++it)   cout << (*it) << " ";  
 }
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
-    solve();
+    file();
+
+    // INPUT
+
+    // END_INPUT
+
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now();
+    #endif
+    
+    while(tc--)  solve();
+    
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "\n[Time: " << duration.count() << " ms]\n"; 
+    #endif
 }
--- map_set_heap/bai8.cpp
+++ map_set_heap/bai8.cpp
@@ -1,76 +1,91 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+
+#define sz(a) (int)a.size()
+#define getbit(x, y) (((x) >> (y)) & 1)
+#define turnon(x, y) ((x) | (1LL << y))
+#define turnof(x, y) ((x) ^ (1LL << y))
+#define foru(i, a, b)   for(int i=a; i<=b; ++i)
+#define ford(i, a, b)   for(int i=a; i>=b; --i)
+#define foruc(i, a, b, c)   for(int i=a; i<=b; i+=c)
+#define fordc(i, a, b, c)   for(int i=a; i>=b; i-=c)
+
 #define fi first
 #define se second
-#define sz(a) (int)a.size()
-using namespace std;
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define lb lower_bound // >=
+#define ub upper_bound // >
 
-int q, m, n;
-int l, r, mid, res;
-vector<pair<int, int>> a;
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
 
-bool check(int x){
-    vector<pair<int, int>> xe;
-    for(int i=1; i<=x; ++i) xe.push_back(a[i]);
-
-    sort(xe.begin(), xe.end());
-
-    int pos = 0;
-    priority_queue<int, vector<int>, greater<int>> qu;
-    for(int i=1; i<=m; ++i){
-        while(pos < sz(xe) && xe[pos].fi <= i){
-            qu.push(xe[pos].se);    
-            pos++;
-        }
-        if(!qu.empty()){
-            int tmp = qu.top();
-            if(tmp < i) return 0;
-            qu.pop();
-        }
-    }
-
-    if(!qu.empty()) return 0;
-    else            return 1;
-
+ll rand(ll l, ll r){
+    assert(l <= r);
+    return l + rd() % (r - l + 1);
 }
-/*
-1
-4 6
-1 2
-1 2
-1 3
-1 3
-2 4
-1 4
 
-1 2 3 4 5 6
+void file(){
+    if(fopen("TEST.INP", "r")){
+        freopen("TEST.INP", "r", stdin);
+        freopen("TEST.OUT", "w", stdout);
+    }
+}
+
+const int mod = 1e9 + 7;
+int tc = 1;
+
+/*
 
 */
+
+int n, m;
+vector<bool> kt;
+vector<ii> val;
+
 void solve(){
-    cin >> m >> n;
+    cin >> n >> m;
+
+    kt = vector<bool>(n + 3, false);
+    val = vector<ii>(m + 3, {0, 0});
+
     for(int i=1; i<=n; ++i){
-        int tm1, tm2;   cin >> tm1 >> tm2;
-        a.push_back({tm1, tm2});
-    }
-    l = 1;
-    r = n;
-    res = 0;
+        if(i == 1)  continue;
 
-    while(l <= r){
-        mid = (l + r) >> 1;
-        if(check(mid)){
-            l = mid + 1;
-            res = max(res, mid);
-        }else   r = mid - 1;
+        if(val[i].fi)
     }
-
-    cout << res << '\n';
 }
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
-    cin >> q;
-    while(q--)  solve();
+    file();
+
+    // INPUT
+
+    // END_INPUT
+
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now();
+    #endif
+    
+    while(tc--)  solve();
+    
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "\n[Time: " << duration.count() << " ms]\n"; 
+    #endif
 }
