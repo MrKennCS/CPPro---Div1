cc29f342ec7a6f7bd792e3f5dc8a2ed21f8eb86b
MrKenne <trungkientuyentinhoc@gmail.comm>
2026-04-06 21:29:44 +0700

    Update from laptop

--- map_set_heap/bai8.cpp
+++ map_set_heap/bai8.cpp
@@ -49,24 +49,73 @@ const int mod = 1e9 + 7;
 int tc = 1;
 
 /*
+1
+4 6
+1 2
+1 2
+1 3
+1 3
+2 4
+1 4
 
 */
 
 int n, m;
-vector<bool> kt;
-vector<ii> val;
+vector<ii> xe(50005, {0, 0});
+int l, r, mid, ans;
+
+bool check(int x){
+    vector<ii> a;
+    for(int i=0; i<x; ++i)  a.pb(xe[i + 1]);
+
+    sort(a.begin(), a.end(), [](const ii& a, const ii& b){
+        if(a.fi == b.fi)    return a.se < b.se;
+        return a.fi < b.fi;
+    });
+
+    int cur = 1;
+    int id = 0;
+
+    pqueue<int, vector<int>, greater<int>> pq;
+
+    while(id < x || !pq.empty()){
+        if(pq.empty())  cur = max(cur, a[id].fi);
+
+        while(id < x && a[id].fi <= cur){
+            pq.push(a[id].se);
+            id++;
+        }
+
+        int mn = pq.top();
+        pq.pop();
+
+        if(cur > n)     return false;
+        if(mn < cur)    return false;
+
+        cur++;
+    }
+
+    return true;
+}
 
 void solve(){
     cin >> n >> m;
+    for(int i=1; i<=m; ++i) cin >> xe[i].fi >> xe[i].se;
 
-    kt = vector<bool>(n + 3, false);
-    val = vector<ii>(m + 3, {0, 0});
+    l = 1;
+    r = m;
 
-    for(int i=1; i<=n; ++i){
-        if(i == 1)  continue;
+    //cout << check(6);
 
-        if(val[i].fi)
+    while(l <= r){
+        mid = l + (r - l) / 2;
+        if(check(mid)){
+            l = mid + 1;
+            ans = mid;
+        }else   r = mid - 1;
     }
+
+    cout << ans << '\n';
 }
 
 int main(){
@@ -80,7 +129,8 @@ int main(){
     #ifndef ONLINE_JUDGE
     auto start = high_resolution_clock::now();
     #endif
-    
+
+    cin >> tc;
     while(tc--)  solve();
     
     #ifndef ONLINE_JUDGE
--- map_set_heap/bai9.cpp
+++ map_set_heap/bai9.cpp
@@ -1,43 +1,80 @@
-#include<iostream>
-#include<set>
-#define sz(a) (int)a.size()
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include<bits/stdc++.h>
+
 using namespace std;
+using namespace std::chrono;
 
-int n, q;
-int a[1009];
-int res, cnt;
-multiset<int> st;
-/*
-3
- 5
- 1 2 3 4 5
- 5
- 4 4 4 4 2
- 6
- 1 3 1 5 3 1
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
 
-1
-6
-1 3 1 5 3 1
-*/
-void solve(){
-    cin >> n;
-    for(int i=1; i<=n; ++i) cin >> a[i];
+#define sz(a) (int)a.size()
+#define getbit(x, y) (((x) >> (y)) & 1)
+#define turnon(x, y) ((x) | (1LL << y))
+#define turnof(x, y) ((x) ^ (1LL << y))
+#define foru(i, a, b)   for(int i=a; i<=b; ++i)
+#define ford(i, a, b)   for(int i=a; i>=b; --i)
+#define foruc(i, a, b, c)   for(int i=a; i<=b; i+=c)
+#define fordc(i, a, b, c)   for(int i=a; i>=b; i-=c)
 
-    for(int i=1; i<=n; ++i){
-        for(int j=1; j<=n-i+1; ++j){
-            st.insert(a[j]);
-            cnt++;
-            if(cnt == i){
-                
-                st.erase(st.find(a[j]));
-            }
-        }
+#define fi first
+#define se second
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define lb lower_bound // >=
+#define ub upper_bound // >
+
+mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
+
+ll rand(ll l, ll r){
+    assert(l <= r);
+    return l + rd() % (r - l + 1);
+}
+
+void file(){
+    if(fopen("TEST.INP", "r")){
+        freopen("TEST.INP", "r", stdin);
+        freopen("TEST.OUT", "w", stdout);
     }
+}
+
+const int mod = 1e9 + 7;
+int tc = 1;
+
+/*
+
+*/
+
+
+
+void solve(){
+
 }
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
-    cin >> q;
-    while(q--)  solve();
+    file();
+
+    // INPUT
+
+    // END_INPUT
+
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now();
+    #endif
+    
+    while(tc--)  solve();
+    
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "\n[Time: " << duration.count() << " ms]\n"; 
+    #endif
 }
