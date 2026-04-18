e2c14ef7e664c74b782f11d78643f176071a566d
MrKenne <trungkientuyentinhoc@gmail.comm>
2026-04-18 14:55:03 +0700

    Update from laptop

---
+++ bitwise_phantap_dequy_quaylui_review/bai29.cpp
@@ -0,0 +1,116 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+
+#define sz(a) (int)a.size()
+#define getbit(x, y) (((x) >> (y)) & 1)
+#define turnon(x, y) ((x) |= (1LL << y))
+#define turnof(x, y) ((x) &= ~(1LL << y))
+#define daobit(x, y) ((x) ^= (1LL << y))
+#define foru(i, a, b)   for(int i=a; i<=b; ++i)
+#define ford(i, a, b)   for(int i=a; i>=b; --i)
+#define foruc(i, a, b, c)   for(int i=a; i<=b; i+=c)
+#define fordc(i, a, b, c)   for(int i=a; i>=b; i-=c)
+
+#define fi first
+#define se second
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define lb lower_bound // >=
+#define ub upper_bound // >
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() ^ random_device{}());
+
+ll randInt(ll l, ll r){
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
+
+void file(){
+    if(fopen("TEST.INP", "r")){
+        freopen("TEST.INP", "r", stdin);
+        freopen("TEST.OUT", "w", stdout);
+    }
+}
+
+const int mod = 1e9 + 7;
+int tc = 1;
+
+/*
+
+*/
+
+string s;
+int n;
+int cnt[130];
+vector<int> state;
+vector<vector<int>> res;
+
+void gen(){
+    if(sz(state) == n){
+        res.pb(state);
+        return ;
+    }
+
+    for(int i=97; i<=122; ++i){
+        if(cnt[i] > 0){
+            state.pb(i);
+            cnt[i]--;
+
+            gen();
+
+            state.popb();
+            cnt[i]++;
+        }
+    }
+}
+
+void solve(){
+    cin >> s;
+    n = sz(s);
+
+    //cout << (int)'a' << " " << (int)'z' << '\n';
+
+    for(int i=0; i<n; ++i)  cnt[s[i]]++;
+
+    gen();
+
+    cout << sz(res) << '\n';
+    for(int i=0; i<sz(res); ++i){
+        for(int j=0; j<n; ++j){
+            cout << (char)res[i][j];
+        }
+        cout << '\n';
+    }
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);   cin.tie(0);
+    file();
+
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now();
+    #endif
+    
+    //cin >> tc;
+    while(tc--)  solve();
+    
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "\n[Time: " << duration.count() << " ms]\n"; 
+    #endif
+}
---
+++ bitwise_phantap_dequy_quaylui_review/bai31.cpp
@@ -0,0 +1,113 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define ull unsigned long long
+#define ii pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+
+#define sz(a) (int)a.size()
+#define getbit(x, y) (((x) >> (y)) & 1)
+#define turnon(x, y) ((x) |= (1LL << y))
+#define turnof(x, y) ((x) &= ~(1LL << y))
+#define daobit(x, y) ((x) ^= (1LL << y))
+#define foru(i, a, b)   for(int i=a; i<=b; ++i)
+#define ford(i, a, b)   for(int i=a; i>=b; --i)
+#define foruc(i, a, b, c)   for(int i=a; i<=b; i+=c)
+#define fordc(i, a, b, c)   for(int i=a; i>=b; i-=c)
+
+#define fi first
+#define se second
+#define pf push_front
+#define pb push_back
+#define popf pop_front
+#define popb pop_back
+#define lb lower_bound // >=
+#define ub upper_bound // >
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count() ^ random_device{}());
+
+ll randInt(ll l, ll r){
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
+
+void file(){
+    if(fopen("TEST.INP", "r")){
+        freopen("TEST.INP", "r", stdin);
+        freopen("TEST.OUT", "w", stdout);
+    }
+}
+
+const int mod = 1e9 + 7;
+int tc = 1;
+
+/*
+
+*/
+
+int a[10][10];
+int res;
+
+void in(int x, int y){
+    for(int i=1; i<=8; ++i){
+        a[x][i] = 0;
+        a[i][y] = 0;
+    }
+    for(int i=x, j=y; i<=8 && j<=8; ++i, ++j)   a[i][j] = 0;
+    for(int i=x, j=y; i<=8 && j<=8; --i, --j)   a[i][j] = 0;
+    for(int i=x, j=y; i<=8 && j<=8; --i, ++j)   a[i][j] = 0;
+    for(int i=x, j=y; i<=8 && j<=8; ++i, --j)   a[i][j] = 0;
+}
+
+void gen(int x){
+    if(x == 9){
+        res++;
+        return ;
+    }
+
+    for(int i=1; i<=8; ++i){
+        for(int j=1; j<=8; ++j){
+            if(!a[i][j])    continue;
+
+
+        }
+    }
+}
+
+void solve(){
+    for(int i=1; i<=8; ++i){
+        for(int j=1; j<=8; ++j){
+            char tmp;   cin >> tmp;
+            a[i][j] = (tmp == '.');
+        }
+    }
+
+    gen(1);
+
+    cout << res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);   cin.tie(0);
+    file();
+
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now();
+    #endif
+    
+    //cin >> tc;
+    while(tc--)  solve();
+    
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "\n[Time: " << duration.count() << " ms]\n"; 
+    #endif
+}
