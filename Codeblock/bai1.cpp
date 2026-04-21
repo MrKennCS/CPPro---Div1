fb0a0cbd7fccef2f9d419eecdbc993f6c060b620
MrKenne <trungkientuyentinhoc@gmail.comm>
2026-04-21 20:45:50 +0700

    Update from laptop

--- Codeblock/bai1.cpp
+++ Codeblock/bai1.cpp
@@ -2,22 +2,14 @@
 
 using namespace std;
 
-void file(){
-    if(fopen("bai1.INP", "r")){
-        freopen("bai1.INP", "r", stdin);
-        freopen("bai1.OUT", "w", stdout);
-    }
-}
-
-int a, b;
-
-void solve(){
-    cin >> a >> b;
-    cout << a << " " << b;
-}
-
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
-    file();
-    solve();
+    freopen("bai1.INP", "r", stdin);
+    freopen("bai1.OUT", "w", stdout);
+
+    int a, b;   cin >> a >> b;
+
+    cout << 1ll * a * b;
+
+    return 0;
 }
--- Codeblock/bai1_trau.cpp
+++ Codeblock/bai1_trau.cpp
@@ -1,12 +1,25 @@
 #include<bits/stdc++.h>
 
+#define ll long long
+
 using namespace std;
+
+void solve(){
+    int a, b;   cin >> a >> b;
+
+    ll ans = 0;
+    for(int i=0; i<a; ++i){
+        for(int j=0; j<b; ++j){
+            ans++;
+        }
+    }
+
+    cout << ans;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);   cin.tie(0);
-    int a, b;   cin >> a >> b;
-
-    cout << 1ll * a * b << '\n';
-
-    return 0;
+    freopen("bai1.INP", "r", stdin);
+    freopen("bai1.ANS", "w", stdout);
+    solve();
 }
